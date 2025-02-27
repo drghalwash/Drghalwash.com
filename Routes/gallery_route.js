8f13a8612e1e50daf321fc8d2913ef5a6b2c7b3f
drghalwash <drkhaledghalwash@gmail.com>
2025-02-27 17:27:25 +0000

    Restored to 'daa9d2c2e7e98360bd0853f530898b1c207d4359'
    Replit-Restored-To:daa9d2c2e7e98360bd0853f530898b1c207d4359

--- Routes/gallery_route.js
+++ Routes/gallery_route.js
@@ -1,13 +1,11 @@
 
 import { Router } from 'express';
 import { index } from '../Controller/gallery.js';
-import { validatePassword, isAuthorized } from '../Controller/galleryAccess.js';
 
 const router = new Router();
 
 router.get('/', index);
 router.get('/:slug', index);
-router.get('/:slug/:subSlug', isAuthorized, index);
-router.post('/validate-password', validatePassword);
+router.get('/:slug/:subSlug', index);
 
 export default router;
